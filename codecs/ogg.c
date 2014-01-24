71ffcac6fb4627b43edbd5bebae8b8ad28cd5be9
Timothy Loiselle <timothy3592@gmail.com>
2014-01-24 01:49:29 -0500

    The ogg module now presents input to vorbisfile. Instead of assigning a pointer to the input buffer to a local variable. It works with modtest if you set BUF_SIZE to something big (eg. 32768).

--- codecs/ogg.c
+++ codecs/ogg.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 #include <vorbis/codec.h>
 #include <vorbis/vorbisfile.h>
 //vorbisfile hack
@@ -17,27 +19,44 @@ struct ogg_data {
 
 struct cb callback;
 
-size_t ogg_read_callback(void *ptr, size_t size, size_t nmemb, void* datasource){
-	//0 == end of data
-	//short reads are allowed
-	struct sp *data = datasource;
-	struct ogg_data *odata = data->private_data;
-
-	int requested_bytes = size * nmemb; //number of bytes requested
-	int bytes_available = data->size - odata->cursor;
-
-	if(bytes_available == -1) return 0; //end of buffer
-
-	//expose buffer input+cursor if the buffer is filled
-	ptr = data->input + odata->cursor;
-	(void)ptr;
-	if(bytes_available > requested_bytes) {
-		odata->cursor += requested_bytes;
-	} else {
-		//odata->cursor += bytes_available;
-		odata->cursor = data->size+1;
+void mcpy(void* dest, void* src, size_t n){
+	//mysterious segfault in __memcpy_sse2_unaligned
+	//to hell with __memcpy_sse2_unaligned
+	char* there = (char*) dest;
+	char* here = (char*) src;
+	while(n--) { //this ought to do it
+		n[there] = n[here];
 	}
-	return bytes_available>requested_bytes ? requested_bytes : bytes_available;
+	0[there] = 0[here];
+}
+
+size_t ogg_read_callback(void *ptr, size_t size, size_t nmemb, void* datasource){
+	//rewrite, because vorbisfile is hard
+	//WAIT... fread never modifies ptr... I've been doing it wrong this whole time
+
+	//retrieve our data 
+	struct sp * env = datasource;
+	struct ogg_data * data = env->private_data;
+
+	int requested = size * nmemb; //number of bytes the user wants
+	int available = env->size - data->cursor; //bytes left in buffer
+
+	//logic it up
+	if( requested <= available ){
+		//we can meet that request
+		memcpy(ptr, env->input + data->cursor, requested);
+		data->cursor += requested;
+		return requested;
+	}
+	else {
+		//we can't meet that request, send what we have
+		memcpy(ptr, env->input + data->cursor, available);
+		data->cursor += available+1; //and +1 for stdiolikeness
+		return available;
+	}
+
+	errno = EAGAIN; //something bad must have happened
+	return 0;
 }
 
 int ogg_init(struct sp* env){
@@ -48,11 +67,21 @@ int ogg_init(struct sp* env){
 	data->handlers.close_func = NULL;
 	data->handlers.tell_func = NULL;
 	data->cursor = 0;
+	int rv = 0; //return value for ogg
 	
-	if (env->input == NULL) return -1; //must have input to successfully open
+	if (env->input == NULL) return SP_AGAIN; //must have input to successfully open
 	//ov_open will pass it's first argument to it's callbacks as the datasource argument
-	//this always fails
-	if (ov_open_callbacks(env, &data->vf, NULL, 0, data->handlers) < 0) return -1;
+	rv = ov_open_callbacks(env, &data->vf, NULL, 0, data->handlers);
+
+	if (rv == OV_EREAD) {
+		//reading input failure... probably needs more data
+		return SP_AGAIN;
+	}
+	if (rv < 0){
+		//not vorbis, corruption, invalid header... bad juju in general
+		return SP_ABORT;
+	}
+	
 	return 0;
 }
 
@@ -61,15 +90,18 @@ int ogg_play(struct sp* env){
 	struct ogg_data *data = env->private_data;
 	int bytes_in_buffer = env->size;
 	int samples;
+	data->cursor = 0;
 	while(bytes_in_buffer){
 		samples = ov_read(&data->vf, buffer, 256, 0, 2, 1, &data->current_section);
 		if(samples > 0) { //no error occured
-			callback.audio_play(buffer, samples/4);
+			callback.audio_play(buffer, samples);
 		}
 		if(samples == 0) break;
 	}
-	data->cursor = 0;
-	return 0;
+
+	//well I can't detect partially used buffers yet
+	//so I'll assume we used it all
+	return env->size;
 }
 
 int ogg_deinit(struct sp* env){
