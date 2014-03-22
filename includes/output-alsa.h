716e989f668453ef5431a3149c8cec1dddfd752e
Timothy Loiselle <timothy3592@gmail.com>
2014-03-22 14:35:31 -0400

    some git housecleaning

--- includes/output-alsa.h
+++
@@ -1,9 +0,0 @@
-#ifndef H_OUTPUT_ALSA
-#define H_OUTPUT_ALSA
-
-int output_alsa_configure(int sample_rate, int channels, void* module_data);
-int output_alsa_init(void* arg);
-int output_alsa_play(char* in, long size);
-int output_alsa_deinit(void);
-
-#endif
