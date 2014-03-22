126ee377b253ca9738499fec4b0fdbd798d88f3a
Timothy Loiselle <timothy3592@gmail.com>
2014-03-22 14:44:59 -0400

    Here are the includes I forgot.

--- includes/alsa.h
+++ includes/alsa.h
@@ -1,9 +1,10 @@
 #ifndef H_OUTPUT_ALSA
 #define H_OUTPUT_ALSA
 
-int output_alsa_configure(int sample_rate, int channels, void* module_data);
-int output_alsa_init(void* arg);
-int output_alsa_play(char* in, long size);
-int output_alsa_deinit(void);
+int alsa(struct sp* env, enum sp_ops operation);
+int alsa_configure(struct sp* env);
+int alsa_init(struct sp* env);
+int alsa_decode(struct sp* env);
+int alsa_deinit(struct sp* env);
 
 #endif
